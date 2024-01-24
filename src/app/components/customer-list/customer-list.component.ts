b341035efab935ce051412db8e2356f9609f97f2
amitmaurya18 <amitmaurya.aps@gmail.com>
2024-01-24 23:49:31 +0530

    search

--- src/app/components/customer-list/customer-list.component.ts
+++ src/app/components/customer-list/customer-list.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { CustomerService } from '../../services/customer/customer.service';
 import { Router } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-customer-list',
   standalone: true,
-  imports: [],
+  imports: [NgbModule],
   templateUrl: './customer-list.component.html',
   styleUrl: './customer-list.component.scss'
 })
 export class CustomerListComponent implements OnInit {
   token: any; 
   customers: any; 
+  originalCustomers: any;
   constructor(private customerService: CustomerService, private router: Router){
 
   }
@@ -57,6 +59,38 @@ export class CustomerListComponent implements OnInit {
           // Handle login error
           console.error(error);
         }
+      );
+    }
+
+    private customerMatchesSearch(customer: any, searchText: string): boolean {
+      // Customize this method based on your search criteria
+      const searchFields = ['firstName', 'lastName', 'street', 'address', 'city', 'state', 'email', 'phone'];
+      
+      return searchFields.some(field =>
+        customer[field].toLowerCase().includes(searchText.toLowerCase())
+      );
+    }
+
+    onSearchChange(event: any) {
+      // Filter the data based on the searchText
+      const searchText = event.target ? event.target.value : '';
+
+      if (!this.originalCustomers) {
+        this.originalCustomers = [...this.customers];
+      }
+    
+      if (searchText.trim() === '') {
+        // If search text is empty, reset to the original data
+        this.customers = [...this.originalCustomers];
+      } else {
+        // Otherwise, filter the data based on the searchText
+        this.customers = this.originalCustomers.filter((customer: any) =>
+          this.customerMatchesSearch(customer, searchText)
+        );
+      }
+
+      this.customers = this.customers.filter((customer: any) =>
+        this.customerMatchesSearch(customer, searchText)
       );
     }
 }
